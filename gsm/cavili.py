82d927dcefd3b80347cbcb7fd5f5c24aec2ab8aa
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-10 16:43:56 +0530

    version change

--- gsm/cavili.py
+++ gsm/cavili.py
@@ -1,8 +1,13 @@
 from sim_modem import Modem, SerialComm
 
-modem = Modem('/dev/ttyACM0')
+comm = SerialComm(address='/dev/ttyACM0', baudrate=460800, timeout=5, at_cmd_delay=0.1,)
+comm.send("ATZ")
+comm.send("ATE1")
+read = comm.read_lines()
+print(read)
+if read[-1] != "OK":
+    raise Exception("Modem do not respond", read)
 
-print(modem)
 
 
 
