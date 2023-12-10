c4a5f41da35fc516af6f689f000aab516d0bb1e6
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-10 15:44:47 +0530

    version change

--- gsm/cavili.py
+++ gsm/cavili.py
@@ -20,7 +20,15 @@ if 'OK' not in response and 'ERROR' in response:
     response = send_command(modem, command)
 
     if 'OK' in response:
-        print('APN added successfully:', apn)
+        lines = response.split('\r\n')
+        print(lines)
+        for line in lines:
+            if '+CGDCONT: 1' in line:
+                parts = line.split(',')
+                if len(parts) > 2:
+                    current_apn = parts[2].strip('\"')
+                    print('Current APN:', current_apn)
+                    break
     else:
         print('Failed to add APN:', apn)
 else:
