e50ee091eb8087f1fa4054cc2bb539bb6b394bb5
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-13 23:18:03 +0530

    version change

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from connectivity import con_status
-# from connectivity.con_status import is_interface_connected
+import os
 from log_helper import log_config
+
+
+def find_gsm_device_type(device_paths):
+    return any(os.path.exists(path) for path in device_paths)
+
 
 if __name__ == '__main__':
     logger, file_handler = log_config.setup_logger()
-    usb_devices = [f"/dev/ttyUSB{i}" for i in range(1, 21)]
-    amc_devices = [f"/dev/ttyAMC{i}" for i in range(1, 21)]
-    gsm_device_type = con_status.find_gsm_device_type(usb_devices + amc_devices)
-    logger.info(f'GSM Device attached to - {gsm_device_type}')
+    usb_device = "/dev/ttyUSB2"
+    amc_device = "/dev/ttyAMC0"
+
+    check_usb = find_gsm_device_type(usb_device)
+    check_amc = find_gsm_device_type(amc_device)
+
+    if check_usb:
+        logger.info(f'GSM Device attached to - {usb_device}')
+    elif check_amc:
+        logger.info(f'GSM Device attached to - {amc_device}')
+    else:
+        logger.info(f'No GSM Device Connected')
+    # gsm_device_type = con_status.find_gsm_device_type(usb_devices + amc_devices)
