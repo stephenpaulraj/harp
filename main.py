1062a8feecd787a8b7a1f76abce10924ddac16a1
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-14 13:42:09 +0530

    version change

--- main.py
+++ main.py
@@ -19,30 +19,24 @@ if __name__ == '__main__':
         active_interface = get_active_network_interface()
         if active_interface:
             logger.info(f"Active network interface: {active_interface}")
+            if active_interface == 'usb0':
+                usb_device = ["/dev/ttyUSB2"]
+                amc_device = ["/dev/ttyACM0"]
+
+                check_usb = find_gsm_device_type(usb_device)
+                check_amc = find_gsm_device_type(amc_device)
+                if check_usb:
+                    logger.info(f'GSM Device attached to - {usb_device[0]}')
+                elif check_amc:
+                    dev = Modem('/dev/ttyACM0')
+                    apn_res = is_valid_apn_configured(dev)
+                    logger.info(f'GSM Device attached to - {amc_device[0]}')
+                    logger.info(f'APN - {apn_res}')
+                else:
+                    logger.info(f'No GSM Device Connected')
         else:
             logger.info("No active network interface found.")
     elif not internet_status:
         logger.info(f'Not connected to Internet')
     else:
         logger.info(f'Internet status not known')
-
-    usb_device = ["/dev/ttyUSB2"]
-    amc_device = ["/dev/ttyACM0"]
-
-    check_usb = find_gsm_device_type(usb_device)
-    check_amc = find_gsm_device_type(amc_device)
-
-    if check_usb:
-        logger.info(f'GSM Device attached to - {usb_device[0]}')
-    elif check_amc:
-        dev = Modem('/dev/ttyACM0')
-        apn_res = is_valid_apn_configured(dev)
-        logger.info(f'GSM Device attached to - {amc_device[0]}')
-        logger.info(f'APN - {apn_res}')
-    else:
-        logger.info(f'No GSM Device Connected')
-
-    # interface_connected = con_status.find_internet_source()
-    # logger.info(f'Source of Internet is: {interface_connected}')
-
-    # gsm_device_type = con_status.find_gsm_device_type(usb_devices + amc_devices)
