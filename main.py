015523500a3cced47540975541e2d8b13325770f
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-17 23:25:57 +0530

    version change

--- main.py
+++ main.py
@@ -34,10 +34,16 @@ if __name__ == '__main__':
     info_collector.collect_info()
     json_data = info_collector.to_json()
 
+    mqtt_instance = MQTTClient(logger)
+    mqtt_thread = threading.Thread(target=lambda: mqtt_instance.client.loop_forever())
+    mqtt_thread.daemon = True
+
     if internet_status:
         mqtt_instance = MQTTClient(logger)
 
         try:
+            mqtt_thread.start()
+
             while True:
                 pass
 
@@ -48,6 +54,7 @@ if __name__ == '__main__':
             mqtt_instance.should_exit = True
             mqtt_instance.client.disconnect()
             mqtt_instance.client.loop_stop()
+            mqtt_thread.join()
 
     elif not internet_status:
         logger.info(f'Not connected to Internet')
