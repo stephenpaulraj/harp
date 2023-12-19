c824f8992a3a848671eabe79515adf6c0f020900
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-19 22:07:05 +0530

    version change

--- main_web_alarm_test.py
+++ main_web_alarm_test.py
@@ -154,22 +154,22 @@ class MQTTClient:
                             i_mod_data = intC(mod_data)
                             if i_mod_data is not None:
                                 self.logger.info(f'Int value : {i_mod_data}')
-                            else :
+                            else:
                                 self.logger.info(f'Int value : 0')
                             # # con_mod_data = self.convertion_for_float(mod_data)
                             # # mod_data = c.read_holding_registers(int(value['Address']), 1)
                             # self.logger.info(f'Int value : {mod_data[0]}')
                             # output_object["value"] = mod_data[0]
                         # elif data_type == 2:
-                            # mod_data = c.read_holding_registers(int(value['Address']) - 1, 1)
-                            # str_data = [str(item) for item in mod_data]
-                            # output_object["value"] = str_data
+                        # mod_data = c.read_holding_registers(int(value['Address']) - 1, 1)
+                        # str_data = [str(item) for item in mod_data]
+                        # output_object["value"] = str_data
                         # elif data_type == 3:
-                            # self.logger.info(f'Address value : {int(value["Address"]) - 1}')
-                            # mod_data = c.read_holding_registers(int(value['Address']) - 1, 2)
-                            # con_mod_data = self.convertion_for_float(mod_data)
-                            # self.logger.info(f'Int value : {con_mod_data[0]}')
-                            # output_object["value"] = con_mod_data[0]
+                        # self.logger.info(f'Address value : {int(value["Address"]) - 1}')
+                        # mod_data = c.read_holding_registers(int(value['Address']) - 1, 2)
+                        # con_mod_data = self.convertion_for_float(mod_data)
+                        # self.logger.info(f'Int value : {con_mod_data[0]}')
+                        # output_object["value"] = con_mod_data[0]
                         # else:
                         #     self.logger.info(f"{key} has an unknown DataType: {data_type}")
                     output_data[key] = output_object
