fca04e51ed9164685e0391748e86651ac9ebfbc0
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-19 22:27:01 +0530

    version change

--- main_web_alarm_test.py
+++ main_web_alarm_test.py
@@ -149,11 +149,11 @@ class MQTTClient:
                         data_type = int(value["DataType"])
                         if data_type == 1:
                             mod_data = c.read_holding_registers(int(value['Address']) - 1, 1)
-                            i_mod_data = intC(mod_data[0])
+                            i_mod_data = intC(mod_data)
                             if i_mod_data is None:
                                 self.logger.info(f'Int value : 0')
                             else:
-                                self.logger.info(f'Int value : {i_mod_data}')
+                                self.logger.info(f'Int value : {i_mod_data[0]}')
 
                             # # con_mod_data = self.convertion_for_float(mod_data)
                             # # mod_data = c.read_holding_registers(int(value['Address']), 1)
@@ -166,11 +166,11 @@ class MQTTClient:
                         elif data_type == 3:
                             mod_data = c.read_holding_registers(int(value['Address']) - 1, 2)
                             con_mod_data = self.convertion_for_float(mod_data)
-                            i_mod_float_data = floatC(con_mod_data[0])
+                            i_mod_float_data = floatC(con_mod_data)
                             if i_mod_float_data is None:
                                 self.logger.info(f'Float value : 0.0')
                             else:
-                                self.logger.info(f'Float value : {i_mod_float_data}')
+                                self.logger.info(f'Float value : {i_mod_float_data[0]}')
                         # else:
                         #     self.logger.info(f"{key} has an unknown DataType: {data_type}")
                     output_data[key] = output_object
