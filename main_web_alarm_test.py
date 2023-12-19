6972f62942ac4190a369af36d1956bf2d9d7b212
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-19 21:25:44 +0530

    version change

--- main_web_alarm_test.py
+++ main_web_alarm_test.py
@@ -154,7 +154,7 @@ class MQTTClient:
                         if data_type == 1:
                             mod_data = c.read_holding_registers(int(value['Address']), 1)
                             self.logger.info(f'Int value : {str(mod_data)}')
-                            output_object["value"] = mod_data[0]
+                            # output_object["value"] = mod_data[0]
                         elif data_type == 2:
                             mod_data = c.read_holding_registers(int(value['Address']), 1)
                             # str_data = [str(item) for item in mod_data]
@@ -163,7 +163,7 @@ class MQTTClient:
                             mod_data = c.read_holding_registers(int(value['Address']), 1 * 2)
                             con_mod_data = self.convertion_for_float(mod_data)
                             self.logger.info(f'Int value : {con_mod_data[0]}')
-                            output_object["value"] = con_mod_data[0]
+                            # output_object["value"] = con_mod_data[0]
                         else:
                             self.logger.info(f"{key} has an unknown DataType: {data_type}")
                     output_data[key] = output_object
