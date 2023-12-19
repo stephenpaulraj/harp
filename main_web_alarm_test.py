c56f83435730c4f190f8d19b9de7adaac045d681
Stephen Paulraj <stephenpaulraj@live.com>
2023-12-19 22:56:21 +0530

    version change

--- main_web_alarm_test.py
+++ main_web_alarm_test.py
@@ -160,14 +160,12 @@ class MQTTClient:
                         # mod_data = c.read_holding_registers(int(value['Address']) - 1, 1)
                         # str_data = [str(item) for item in mod_data]
                         # output_object["value"] = str_data
-                        # elif data_type == 3:
-                        #     mod_data = c.read_holding_registers(int(value['Address']) - 1, 2)
-                        #     con_mod_data = self.convertion_for_float(mod_data)
-                        #     i_mod_float_data = floatC(con_mod_data)
-                        #     if i_mod_float_data is None:
-                        #         self.logger.info(f'Float value : 0.0')
-                        #     else:
-                        #         self.logger.info(f'Float value : {i_mod_float_data[0]}')
+                        elif data_type == 3:
+                            mod_data = c.read_holding_registers(int(value['Address']) - 1, 2)
+                            con_mod_data = self.convertion_for_float(mod_data)
+                            float_string = [str(value) if value is not None else '0.0' for value in con_mod_data]
+                            result_string = ', '.join(float_string)
+                            self.logger.info(f'Float values: {result_string}')
                         # else:
                         #     self.logger.info(f"{key} has an unknown DataType: {data_type}")
                     output_data[key] = output_object
