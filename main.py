8e483814fc823aa57542a46f4b503cc06cb0b54b
Stephen Paulraj <stephenpaulraj@live.com>
2024-01-10 19:30:27 +0530

    version change 1

--- main.py
+++ main.py
@@ -180,16 +180,22 @@ class MQTTClient:
                         }
                     }
                 )
-                device_info_obj = DeviceInformation()
-                device_info_obj.get_device_info()
-                dev_payload = device_info_obj.to_json()
+                # device_info_obj = DeviceInformation()
+                # device_info_obj.get_device_info()
+                # dev_payload = device_info_obj.to_json()
 
-                if self.is_eth1_interface_present():
-                    result, mid = self.client.publish("iot-data3", payload=payload, qos=1, retain=True)
-                    if result == mqtt.MQTT_ERR_SUCCESS:
-                        self.logger.info(f"Connection Payload send! Message ID: {mid}")
-                    else:
-                        self.logger.error(f"Error sending Connection Payload! MQTT Error Code: {result}")
+                result, mid = self.client.publish("iot-data3", payload=payload, qos=1, retain=True)
+                if result == mqtt.MQTT_ERR_SUCCESS:
+                    self.logger.info(f"Connection Payload send! Message ID: {mid}")
+                else:
+                    self.logger.error(f"Error sending Connection Payload! MQTT Error Code: {result}")
+
+                # if self.is_eth1_interface_present():
+                #     result, mid = self.client.publish("iot-data3", payload=payload, qos=1, retain=True)
+                #     if result == mqtt.MQTT_ERR_SUCCESS:
+                #         self.logger.info(f"Connection Payload send! Message ID: {mid}")
+                #     else:
+                #         self.logger.error(f"Error sending Connection Payload! MQTT Error Code: {result}")
 
                     # result, mid = self.client.publish("iot-data3", payload=test_function_ss(self.c), qos=1, retain=True)
                     # if result == mqtt.MQTT_ERR_SUCCESS:
@@ -197,23 +203,23 @@ class MQTTClient:
                     # else:
                     #     self.logger.error(f"Error sending PLC Payload! MQTT Error Code: {result}")
 
-                    result, mid = self.client.publish("dev-data", payload=dev_payload, qos=1, retain=True)
-                    if result == mqtt.MQTT_ERR_SUCCESS:
-                        self.logger.info(f"Device_info Payload send! Message ID: {mid}")
-                    else:
-                        self.logger.error(f"Error sending Device_info Payload! MQTT Error Code: {result}")
-                else:
-                    result, mid = self.client.publish("iot-data3", payload=payload, qos=1, retain=True)
-                    if result == mqtt.MQTT_ERR_SUCCESS:
-                        self.logger.info(f"Connection Payload send! Message ID: {mid}")
-                    else:
-                        self.logger.error(f"Error sending Connection Payload! MQTT Error Code: {result}")
-
-                    result, mid = self.client.publish("dev-data", payload=dev_payload, qos=1, retain=True)
-                    if result == mqtt.MQTT_ERR_SUCCESS:
-                        self.logger.info(f"Device_info Payload send! Message ID: {mid}")
-                    else:
-                        self.logger.error(f"Error sending Device_info Payload! MQTT Error Code: {result}")
+                    # result, mid = self.client.publish("dev-data", payload=dev_payload, qos=1, retain=True)
+                    # if result == mqtt.MQTT_ERR_SUCCESS:
+                    #     self.logger.info(f"Device_info Payload send! Message ID: {mid}")
+                    # else:
+                    #     self.logger.error(f"Error sending Device_info Payload! MQTT Error Code: {result}")
+                # else:
+                #     result, mid = self.client.publish("iot-data3", payload=payload, qos=1, retain=True)
+                #     if result == mqtt.MQTT_ERR_SUCCESS:
+                #         self.logger.info(f"Connection Payload send! Message ID: {mid}")
+                #     else:
+                #         self.logger.error(f"Error sending Connection Payload! MQTT Error Code: {result}")
+                #
+                #     result, mid = self.client.publish("dev-data", payload=dev_payload, qos=1, retain=True)
+                #     if result == mqtt.MQTT_ERR_SUCCESS:
+                #         self.logger.info(f"Device_info Payload send! Message ID: {mid}")
+                #     else:
+                #         self.logger.error(f"Error sending Device_info Payload! MQTT Error Code: {result}")
 
     def process_remote_access(self, msg):
         m_decode = str(msg.payload.decode("UTF-8", "ignore"))
