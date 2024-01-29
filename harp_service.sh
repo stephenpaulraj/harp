c39a361feaa62e03b13233147c61fd5a16c1cb96
Stephen Paulraj <stephenpaulraj@live.com>
2024-01-29 16:39:52 +0530

    version change 2

--- harp_service.sh
+++ harp_service.sh
@@ -7,31 +7,12 @@ GITHUB_REPO="https://github.com/stephenpaulraj/harp"
 GITHUB_VERSION_FILE="https://raw.githubusercontent.com/stephenpaulraj/harp/main/version.txt"
 TEMP_FOLDER="/home/pi/temp"
 
-sudo pon rnet
-
-route_found=$(/sbin/route -n | /bin/grep -c ^0.0.0.0)
-#echo route_found
-
-ppp_on=$(/sbin/ifconfig | /bin/grep -c ppp0)
-
-#echo ppp_on
-
-if [ $route_found -eq 1 ] && [ $ppp_on -eq 1 ]
-  #then /sbin/route add default ppp0
-  then sudo route add default dev ppp0
-fi
-
-#sudo route add default dev ppp0
-
-
-
 echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
 service networking restart
 
-
 sudo apt-get update
 sudo apt-get install -y python3-pip git python3-venv
-#sudo pip3 install virtualenv
+
 if [ ! -d ${HARP_FOLDER} ]; then
     mkdir ${HARP_FOLDER}
     python3 -m venv ${VENV_FOLDER}
@@ -39,7 +20,6 @@ fi
 
 if [ ! -d ${TEMP_FOLDER} ]; then
     mkdir ${TEMP_FOLDER}
-
 fi
 
 check_tun0() {
