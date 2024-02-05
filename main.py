93f897580a586164f77ebf9cf9960d650c2d94d6
Stephen Paulraj <stephenpaulraj@live.com>
2024-02-05 22:38:34 +0530

    version change 2

--- main.py
+++ main.py
@@ -80,8 +80,8 @@ class MQTTClient:
     def is_eth1_interface_present(self):
         with IPDB() as ipr:
             try:
-                eth1_interface = ipr.interfaces.eth1.operstate
-                if eth1_interface != "UP":
+                eth1_operstate = ipr.interfaces.eth1.operstate
+                if eth1_operstate != "UP":
                     self.logger.error("eth1 interface not found or not UP.")
                     return False
 
